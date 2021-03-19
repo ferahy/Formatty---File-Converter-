41c4bd0e60fc3aafc37d96b1376598a27bb17637
matthewdalby <47013011+matthewdalby@users.noreply.github.com>
2021-03-18 18:06:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, send_file
 from flask_sqlalchemy import SQLAlchemy
 import templates
-from app.forms import LoginForm
+#from app.forms import LoginForm
+from io import BytesIO
+
 
 app = Flask(__name__)
 
@@ -20,13 +22,27 @@ class FileContents(db.Model):
 def home():
     return render_template('home.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    file = request.files['file']
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
+@app.route("/file_list")
+def file_list():
+    database = FileContents.query.all()
+    return render_template('file_list.html', data = database)
+
+   
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['inputFile']   
     newFile = FileContents(name=file.filename, data=file.read())
-    #db.add_file(newFile)
     db.session.add(newFile)
     db.session.commit()
+    
+    return 'Saved' + file.filename + ' to the database'
+    
 
-    return 'Saved ' + file.filename + ' to the database'
+@app.route('/download/<int:file_id>')
+def download(file_id):
+    file_data = FileContents.query.filter_by(id=file_id).first()
+    return send_file(BytesIO(file_data.data), attachment_filename = 'test2.pdf', as_attachment=True)
